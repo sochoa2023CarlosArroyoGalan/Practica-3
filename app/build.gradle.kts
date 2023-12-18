dd66cc4f2b9ec773d12b3f65201f1591fd810c5f
Carlos <erentitan4512@gmail.com>
2023-12-18 17:19:30 +0100

    Punto 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "net.iessochoa.carlosarroyogalan.practica3"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "net.iessochoa.carlosarroyogalan.practica3"
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    //Añadimos al fichero build.grapple
+    buildFeatures{
+        viewBinding = true
     }
 }
 
--- app/src/main/java/net/iessochoa/carlosarroyogalan/practica3/MainActivity.kt
+++ app/src/main/java/net/iessochoa/carlosarroyogalan/practica3/MainActivity.kt
@@ -1,11 +1,27 @@
 package net.iessochoa.carlosarroyogalan.practica3
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.iessochoa.carlosarroyogalan.practica3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Definimos la instancia Binding de la clase
+    private lateinit var binding: ActivityMainBinding
+    var num:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Se elimina el inflado y se sustituye, permitiendonos acceder a los views del layout
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Nos permite inicializar tvNumero
+        binding.tvNumero.text=num.toString()
+        //Asignamos la acción de sumar 1 al contenido de tvNumero
+        binding.btSumaUno.setOnClickListener(){
+            num++
+            binding.tvNumero.text=num.toString()
+        }
     }
 }
