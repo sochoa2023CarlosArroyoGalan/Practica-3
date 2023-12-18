2a83c06c70c15ea8cf0db69c4257ad1df6f3895b
Carlos <erentitan4512@gmail.com>
2023-12-18 17:24:08 +0100

    Punto 18

--- app/src/main/java/net/iessochoa/carlosarroyogalan/practica3/MainActivity.kt
+++ app/src/main/java/net/iessochoa/carlosarroyogalan/practica3/MainActivity.kt
@@ -3,14 +3,18 @@ package net.iessochoa.carlosarroyogalan.practica3
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import net.iessochoa.carlosarroyogalan.practica3.databinding.ActivityMainBinding
-
+//Nos permite localizar los mensajes de nuestra app en el logcat
+private const val TAG = "practica3"
 class MainActivity : AppCompatActivity() {
     //Definimos la instancia Binding de la clase
     private lateinit var binding: ActivityMainBinding
     var num:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Enviara un mensaje al logcat cuando se ejecute
+        Log.i(TAG, "onCreate")
         //Se elimina el inflado y se sustituye, permitiendonos acceder a los views del layout
         // setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,5 +27,30 @@ class MainActivity : AppCompatActivity() {
             num++
             binding.tvNumero.text=num.toString()
         }
+    }
+    //Enviaran mensajes al logcat
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        Log.i(TAG, "onPause")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i(TAG, "onStop")
+        super.onStop()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy")
+        super.onDestroy()
     }
 }
